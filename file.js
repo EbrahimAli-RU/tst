4978d206017ad37c351a36ebb8a934e150c3a873
Md. Ebrahim Ali <eali@mymedicalhub.com>
2022-12-28 00:12:41 +0600

    hjh

---
+++ file.js
@@ -0,0 +1,9 @@
+const person = {
+    userName: "Ebrahim"
+}
+const {userName} = person
+console.log(userName)
+
+person.userName = "Ali"
+
+console.log(userName)
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useRef, useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import InputMask from 'react-input-mask';
+import { createEnvFile } from "../file"
+
+const App = (props) => {
+  const [val, setVal] = useState('')
+  const gghgh = useRef(null)
+  useEffect(() => {
+    console.log(gghgh)
+    createEnvFile();
+  }, [])
+  const onChangeInputText = (e) => {
+    console.log(e)
+    setVal(e.target.value)
+  }
+  return (
+    <div>
+      <h1>HELLO</h1>
+      <InputMask
+          // id={'input-box-' + chat.id}
+          mask="99/99/9999"
+          type="text"
+          placeholder="Please enter your date of birth"
+          autoComplete="off"
+          className="form-control w-50"
+          value={val}
+          // inputRef={input => {
+          //   console.log(input)
+          //   console.log(input.focus)
+          // }}
+          onChange={(e) => onChangeInputText(e)}
+          // onKeyPress={onKeyPressHandleEnter(chat)}
+      >
+        {inputProps => <input ref={input => input && input.focus && input.focus()} placeholder="HJHJHJHJHJ" className="form-control w-50" />}
+      </InputMask>
+    </div>
+  );
+};
+
+export default App;
